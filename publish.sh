a3bf5f5ee6363619ff08b38a2650d55a9745f398
Simon Schwitz <simon@calucon.de>
2022-08-11 02:29:39 +0200

    added images to distribution

--- publish.sh
+++ publish.sh
@@ -6,6 +6,9 @@ rm -rf dist/
 mkdir -p dist/
 cp src/* dist/ -r
 
+mkdir -p dist/img/
+cp img/* dist/img/ -r
+
 npm i
 ./node_modules/sass/sass.js --style=compressed scss/index.scss:dist/index.min.css
 ./node_modules/.bin/webpack --mode=production
