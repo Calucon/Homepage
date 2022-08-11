cb3037ede44afa83271b6e123ab9cd0de0f5b7b7
Simon Schwitz <simon@calucon.de>
2022-08-11 03:46:04 +0200

    publish FA webfonts

--- publish.sh
+++ publish.sh
@@ -12,3 +12,6 @@ cp img/* dist/img/ -r
 npm i
 ./node_modules/sass/sass.js --style=compressed scss/index.scss:dist/index.min.css
 ./node_modules/.bin/webpack --mode=production
+
+mkdir -p dist/webfonts/
+cp './node_modules/@font-awesome/fontawesome-free/webfonts/*' dist/webfonts/
