d2e5d4c82fe979b7530d13e954c782aa543a5b38
Simon Schwitz <simon@calucon.de>
2022-08-11 02:28:19 +0200

    removed old css/js imports

--- src/index.php
+++ src/index.php
@@ -68,11 +68,8 @@ function loadAllPosts(){
     <link rel="image_src" href="./favicon.ico">
 
     <!-- Site CSS and JS -->
-    <link rel="stylesheet" href="/css/bulma.min.css">
-    <link rel="stylesheet" href="/css/bulma-social.min.css">
-    <link rel="stylesheet" href="/css/bulma-timeline.min.css">
-    <link rel="stylesheet" href="/css/fontawesome/css/all.css">
-    <script src="/js/main.js"></script>
+    <link rel="stylesheet" href="index.min.css">
+    <script src="index.bundle.js"></script>
 </head>
 <body>
     <?php     
