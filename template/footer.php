a7c077851484284116be139611bf2d37f53794f7
Simon Schwitz <simon@calucon.de>
2023-01-13 20:49:38 +0100

    updated styling #3

--- template/footer.php
+++ template/footer.php
@@ -1,5 +1,5 @@
-<footer class="footer has-background-white-ter">
-    <div class="container has-text-centered">
+<footer class="footer has-background-dark">
+    <div class="container has-text-centered has-text-white-ter">
         &copy; <?php echo (new DateTime('now', new DateTimeZone('utc')))->format('Y'); ?> Calucon / Simon Schwitz
     </div>
 </footer>
