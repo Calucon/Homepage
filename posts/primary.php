ddb4b2edd9c9a8c6d3cfb97116cb079f285b5309
Simon Schwitz <simon@calucon.de>
2024-03-10 17:47:07 +0100

    updated about-me text

--- posts/primary.php
+++ posts/primary.php
@@ -10,13 +10,17 @@
             I'm currently studying
             <a href="https://www.informatik.uni-konstanz.de/studium/master-of-science/msc-computer-and-information-science/study/master-of-science/master-computer-and-information-science/" target="_blank" rel="noopener">Computer and Information Science (M. Sc.)</a>
             at the <a href="https://www.uni-konstanz.de/en/" target="_blank" rel="noopener">University of Konstanz</a> and employed as a full-stack developer at <a href="https://fast-rocket.de/" target="_blank" rel="noopener">FastRocket GmbH</a>.
-        </p><br />
+        </p>
+        <br />
         <p>
+            <strong>University</strong><br />
             In my university studies, I am specializing in the field of Human-Computer Interaction (HCI).
             I have a particular passion for crafting mixed-reality interactions and interfaces, and I am eager to deepen my understanding and expertise in this domain.
-        </p><br />
+        </p>
+        <br />
         <p>
-            At FastRocket GmbH, my pcore responsibility revolves around the development of diverse PHP applications, encompassing platforms such as WordPress and Shopware.
+            <strong>FastRocket GmbH</strong><br />
+            At FastRocket GmbH, my core responsibility revolves around the development of diverse PHP applications, encompassing platforms such as WordPress and Shopware.
             The primary objective of our company is to advance our clients to the forefront of digitalization through bespoke web applications and mobile apps.
         </p>
     </div>
