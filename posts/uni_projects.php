9d5aef1bda58674c35443fdd774bb8b22e9f6ec6
Simon Schwitz <simon@calucon.de>
2024-12-04 23:46:20 +0100

    added physcomp project

--- posts/uni_projects.php
+++ posts/uni_projects.php
@@ -20,11 +20,40 @@
         </p>
         <br />
         <p>
-            <strong>Architecture Manipulation</strong>
+            <strong>Architecture Manipulation</strong><br />
             This redirection technique heavily influences the level design. Our goal was to provide a virtual environment where the user had to walk through multiple doors. Whenever the user was distracted by opening or closing a door, the world would simultaneously rotate around them.
         </p>
         <hr />
         <a class="button is-small is-youtube" href="https://www.youtube.com/watch?v=0cR6ZS_KPUI" target="_blank" rel="noopener">
+            <span class="icon is-small">
+                <i class="fab fa-lg fa-youtube"></i>
+            </span>
+            <span>Watch Project Showcase</span>
+        </a>
+    </div>
+</div>
+<div class="card" id="uni-tactile-time-machine">
+    <header class="card-header has-background-success-light">
+        <p class="card-header-title">
+            Tactile Time Machine
+        </p>
+    </header>
+    <div class="card-content">
+        <div class="columns">
+            <div class="column">
+                <p>The Tactile Time Machine was the resulting prototype of the "Physical Computing" lecture. The objective of the lecture was to build a working hardware prototype throughout the semester. During the first lecture, our professor showed us where we could get e.g. cardboard boxes for prototyping. There she found an old radio that was thrown out. My group partner and I took this golden opportunity and decided to give this radio a second life.</p>
+            </div>
+            <div class="column is-one-third">
+                <figure class="image is-3by1">
+                    <img src="/img/radio_prototype.webp" class="has-ratio img-rounded" alt="Radio Prototype with sketches" loading="lazy" />
+                </figure>
+            </div>
+        </div>
+        <p>Over the subsequent weeks, we came up with the plan to repurpose the frequency selection wheel of the radio and allow the user to switch years instead of frequencies. The selection wheel was then coupled with a digital display to show the user the currently selected year. The buttons of the radio were repurposed to allow switching the device on or off as well as selecting a random song that was popular in the currently selected year.</p><br />
+        <p>As an additional gimmick, we added a display on top of the devices that allows the user to browse a (popular) website during that time.</p>
+        <p>To provide additional atmosphere, we added two light bulbs emitting warm wight light on both sides. The further the user goes back in time the dimmer the bulbs get. </p>
+        <hr />
+        <a class="button is-small is-youtube" href="https://www.youtube.com/watch?v=adV6gOKDUYg" target="_blank" rel="noopener">
             <span class="icon is-small">
                 <i class="fab fa-lg fa-youtube"></i>
             </span>
